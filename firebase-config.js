464e38049a3388001d645c2499a1ccdeb5c22827
SABI9666 <sabincn676@gmail.com>
2025-07-21 11:17:38 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js';
+import { getStorage } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAJHUN9Dv1T9hE-wCg9yb16w9vAVqFvFIw",
+  authDomain: "steel-connect-f6013.firebaseapp.com",
+  projectId: "steel-connect-f6013", 
+  storageBucket: "steel-connect-f6013.firebasestorage.app",
+  messagingSenderId: "345092477343",
+  appId: "G-JCJJDHTQ7T"
+};
+
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
