793f82b4fce3d6257816d025c9755668081bc15f
SABI9666 <sabincn676@gmail.com>
2025-07-21 14:20:11 +0530

    Update page.js

--- page.js
+++ page.js
@@ -1,32 +1,13 @@
-// For Pages Router: pages/upload.js
-// For App Router: app/upload/page.js
-
-import FileUpload from '../components/FileUpload';
-
-export default function UploadPage() {
-  return (
-    <div>
-      <head>
-        <title>File Upload - Steel Connect</title>
-        <meta name="description" content="Upload files to Firebase Storage" />
-      </head>
-      
-      <main>
-        <FileUpload />
-      </main>
-    </div>
-  );
-}
-
-// If using App Router (app/upload/page.js), use this instead:
-/*
 import FileUpload from '../../components/FileUpload';
 
 export default function UploadPage() {
   return (
-    <main>
+    <main className="main-container">
+      <head>
+        <title>File Upload - Steel Connect</title>
+        <meta name="description" content="Upload files to Firebase Storage" />
+      </head>
       <FileUpload />
     </main>
   );
 }
-*/
